7342688eda1f9e1baa438ecd2141bc67acd92827
Damien Tabary <damtab83@gmail.com>
2023-11-12 00:20:41 +0100

    creating hours

--- src/Controller/Admin/OpeningHoursCrudController.php
+++ src/Controller/Admin/OpeningHoursCrudController.php
@@ -22,7 +22,7 @@ class OpeningHoursCrudController extends AbstractCrudController
     {
         return $actions
             ->remove(Crud::PAGE_INDEX, Action::NEW)
-            ->remove(Crud::PAGE_INDEX, Action::DELETE);
+            // ->remove(Crud::PAGE_INDEX, Action::DELETE);
     }
 
     public function configureCrud(Crud $crud): Crud
