a2792db99e15ce60dc26f1d88ebbfffdc0027095
Damien Tabary <damtab83@gmail.com>
2023-11-12 12:48:01 +0100

    ROLE_SELLER placed

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -76,7 +76,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        $roles[] = 'ROLE_ADMIN';
+        $roles[] = 'ROLE_SELLER';
 
         return array_unique($roles);
     }
