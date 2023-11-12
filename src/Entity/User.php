13450469de2f7300727c6899733146ac41c485f7
Damien Tabary <damtab83@gmail.com>
2023-11-12 20:25:59 +0100

    restored role admin

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -76,7 +76,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        $roles[] = ['ROLE_SELLER'];
+        $roles[] = ['ROLE_ADMIN'];
 
         return array_unique($roles);
     }
